2d46d77b2f0af7676757939fce40748fb163cd1d
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2020-05-07 14:28:17 -0500

    Update e1.java

--- e1.java
+++ e1.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class Program
 {
     public static void main(String[] args) {
+        
+        int n;
         Scanner scan = new Scanner(System.in);
         System.out.println("Ingresa un numero");
-        int n = scan.nextInt();
+        n = scan.nextInt();
         
         if(n>0){
             System.out.println("El numero es positivo");
