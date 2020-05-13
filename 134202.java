8ce1c1d4bb09b6d0368e55472a4879f8e7b26fb7
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2020-05-13 15:19:05 -0500

    Update 134202.java

--- 134202.java
+++ 134202.java
@@ -14,22 +14,22 @@ public class Program
         switch(CATE){
             case 1:
             SUE = SUE*1.15;
-            System.out.println("Su categoria es 1 y su sueldo es " + SUE)
+            System.out.println("Su categoria es 1 y su sueldo es " + SUE);
             break;
             
             case 2:
             SUE = SUE*1.1;
-            System.out.println("Su categoria es 2 y su sueldo es " + SUE)
+            System.out.println("Su categoria es 2 y su sueldo es " + SUE);
             break;
             
             case 3:
             SUE = SUE*1.08;
-            System.out.println("Su categoria es 3 y su sueldo es " + SUE)
+            System.out.println("Su categoria es 3 y su sueldo es " + SUE);
             break;
             
             case 4:
             SUE = SUE*1.07;
-            System.out.println("Su categoria es 4 y su sueldo es " + SUE)
+            System.out.println("Su categoria es 4 y su sueldo es " + SUE);
             break;
         }
     }
