f9332047d7277e6b612be9a03b4a0a6ebfd12829
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2020-05-21 13:16:04 -0500

    Create TAREAS06s1.java

---
+++ TAREAS06s1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Program
+{
+    public static void main(String[] args) {
+        int num, i = 0, suma_par = 0, suma_impar = 0, cont_par = 0;
+        float prom = 0;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Ingresa el numero");
+        num = scan.nextInt();
+        
+        while(i<=270){
+            i++;
+            if(num%2 = 0){
+                suma_par = suma_par + num;
+                cont_par++;
+            }else{
+                suma_impar = suma_impar + num;
+            }
+            System.out.println("Ingresa el numero");
+            num = scan.nextInt();
+        }
+        prom = suma_par/cont_par;
+        System.out.println("La suma de los numeros impares es: " + suma_impar);
+        System.out.println("El promedio de los numeros pares es: " + prom);
+    }
+}
