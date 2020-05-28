b11a043e0131e907104a621d41c05f1d459195f9
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2020-05-28 15:18:43 -0500

    Create Ejercicio2S7s2.java

---
+++ Ejercicio2S7s2.java
@@ -0,0 +1,11 @@
+public class Encuentro{
+    public static void main(String[] args) {
+    
+        int k1, k2, distancia, encuentro;
+        k1 = 70;
+        k2 = 150;
+        distancia = k2 - k1;
+        encuentro = distancia/2 + 70;
+        System.out.println("Se encontraran en el kilometro " + encuentro);
+    }
+}
