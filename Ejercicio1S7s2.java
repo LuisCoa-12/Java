ceb19c2b22e47a5c971962c99bdfa54227f2f9bc
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2020-05-28 15:02:11 -0500

    Update Ejercicio1S7s2.java

--- Ejercicio1S7s2.java
+++ Ejercicio1S7s2.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 public class Fibonacii{
     public static void main(String[] args) {
-    
-    Scanner scan = new Scanner(System.in);
-    
-    int elementos, a, b, c, i;
-    a = 0;
-    b = 1;
-    c = 1;
-    i = 1;
-    
-    System.out.println("Ingresa el numero de elementos para la sucesion: ");
-    elementos = scan.nextInt();
-    do{
-        System.out.println(a);
-        c = a + b;
-        a = b;
-        b = c;
-        i++;
-    }while(i<=elementos);
+
+        Scanner scan = new Scanner(System.in);
+
+        int elementos, a, b, c, i;
+        a = 0;
+        b = 1;
+        c = 1;
+        i = 1;
+
+        System.out.println("Ingresa el numero de elementos para la sucesion: ");
+        elementos = scan.nextInt();
+        do{
+            System.out.println(a);
+            c = a + b;
+            a = b;
+            b = c;
+            i++;
+        }while(i<=elementos);
     }
 }
