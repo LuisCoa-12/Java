4ba9934a81354ec21beb840abf933597cfdc359c
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2020-05-28 14:21:54 -0500

    Create TareaS07.java

---
+++ TareaS07.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Program{
+    public static void main(String[] args) {
+        int contador, ahorro, cantidad;
+        contador = 1;
+        ahorro = 0;
+        Scanner scan = new Scanner(System.in);
+        
+        do{
+            System.out.println("Ingresar el monto mensual");
+            cantidad = scan.nextInt();
+            
+            ahorro = ahorro + cantidad;
+            System.out.println("El ahorro del mes " + contador + " es " + ahorro);
+            contador++;
+        }while(contador<=12);
+    }
+}
